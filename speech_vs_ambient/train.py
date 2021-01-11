676268bb5312aab06f38c5405337af6f9bf18953
hushell <dom343@gmail.com>
2021-01-11 08:56:39 -0600

    add losses

--- speech_vs_ambient/train.py
+++ speech_vs_ambient/train.py
@@ -12,12 +12,12 @@ from pytorch_lightning.callbacks import ModelCheckpoint, EarlyStopping
 from asteroid.engine.optimizers import make_optimizer
 from asteroid.engine.schedulers import DPTNetScheduler
 from asteroid.engine.system import System
-from asteroid.losses.sdr import SingleSrcNegSDR
 
 sys.path.append("../")
 
 from src.librimix_dataset import LibriMix
 from src.model import AvaTr
+from src.utils import SingleSrcNegSDR
 
 
 # Additional arguments
