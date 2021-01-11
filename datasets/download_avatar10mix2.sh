fb683d4ba96ea4981db0d3b0042fca3697263307
hushell <dom343@gmail.com>
2021-01-11 11:53:16 +0100

    fix metadata

--- datasets/download_avatar10mix2.sh
+++ datasets/download_avatar10mix2.sh
@@ -1,3 +1,14 @@
 wget https://www.dropbox.com/s/jg2og2ogf0l7ke9/Avatar10Mix2.tar.gz?dl=0
 mv Avatar10Mix2.tar.gz?dl=0 Avatar10Mix2.tar.gz
 tar zxvf Avatar10Mix2.tar.gz
+
+# pre-processing
+cd Avatar10Mix2/metadata/
+find . -iname "mixture*" | wc
+
+for filename in `find . -iname "mixture*"`
+do
+    sed -i 's/Avatar10Mix2\///g' $filename
+done
+
+cd -
