23223356738917562afec66f7718c0f2db52ae77
hushell <dom343@gmail.com>
2021-01-17 23:36:49 +0100

    tweaks

--- src/model_planb.py
+++ src/model_planb.py
@@ -13,6 +13,7 @@ from typing import Optional, List
 import torch
 import torch.nn.functional as F
 from torch import nn, Tensor
+from .light_wav2vec2 import Wav2Vec2Model
 
 
 class AvaTr(nn.Module):
